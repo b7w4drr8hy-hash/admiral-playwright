e9bdccc716bf3e785331485360f224b9bb3bcf5e
b7w4drr8hy-hash <b7w4drr8hy@privaterelay.appleid.com>
2026-05-26 02:00:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -45,4 +45,9 @@ def events():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        threaded=False
+    )
+
